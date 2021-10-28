3aa937e9d7acaebc46b0933154c05687c2bd839c
Alex Stansfield <alex.stansfield@gmail.com>
2021-10-28 17:22:15 +0700

    Added support for Fastify requests

--- src/decorator.spec.ts
+++ src/decorator.spec.ts
@@ -1,6 +1,7 @@
 import { ROUTE_ARGS_METADATA } from '@nestjs/common/constants'
 import { HttpArgumentsHost, CustomParamFactory, ExecutionContext } from '@nestjs/common/interfaces'
-import { Request } from 'express'
+import { Request as ExpressRequest } from 'express'
+import { FastifyRequest } from 'fastify'
 import { Paginate, PaginateQuery } from './decorator'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -15,16 +16,31 @@ function getParamDecoratorFactory<T>(decorator: Function): CustomParamFactory {
 }
 const decoratorfactory = getParamDecoratorFactory<PaginateQuery>(Paginate)
 
-function contextFactory(query: Request['query']): Partial<ExecutionContext> {
+function expressContextFactory(query: ExpressRequest['query']): Partial<ExecutionContext> {
     const mockContext: Partial<ExecutionContext> = {
         switchToHttp: (): HttpArgumentsHost =>
             Object({
-                getRequest: (): Partial<Request> =>
+                getRequest: (): Partial<ExpressRequest> =>
                     Object({
                         protocol: 'http',
                         get: () => 'localhost',
-                        baseUrl: '',
-                        path: '/items',
+                        originalUrl: '/items?search=2423',
+                        query: query,
+                    }),
+            }),
+    }
+    return mockContext
+}
+
+function fastifyContextFactory(query: FastifyRequest['query']): Partial<ExecutionContext> {
+    const mockContext: Partial<ExecutionContext> = {
+        switchToHttp: (): HttpArgumentsHost =>
+            Object({
+                getRequest: (): Partial<FastifyRequest> =>
+                    Object({
+                        protocol: 'http',
+                        hostname: 'localhost',
+                        url: '/items?search=2423',
                         query: query,
                     }),
             }),
@@ -33,8 +49,8 @@ function contextFactory(query: Request['query']): Partial<ExecutionContext> {
 }
 
 describe('Decorator', () => {
-    it('should handle undefined query fields', () => {
-        const context = contextFactory({})
+    it('should handle express undefined query fields', () => {
+        const context = expressContextFactory({})
 
         const result: PaginateQuery = decoratorfactory(null, context)
 
@@ -49,8 +65,53 @@ describe('Decorator', () => {
         })
     })
 
-    it('should handle defined query fields', () => {
-        const context = contextFactory({
+    it('should handle fastify undefined query fields', () => {
+        const context = fastifyContextFactory({})
+
+        const result: PaginateQuery = decoratorfactory(null, context)
+
+        expect(result).toStrictEqual({
+            page: undefined,
+            limit: undefined,
+            sortBy: undefined,
+            search: undefined,
+            searchBy: undefined,
+            filter: undefined,
+            path: 'http://localhost/items',
+        })
+    })
+
+    it('should handle express defined query fields', () => {
+        const context = expressContextFactory({
+            page: '1',
+            limit: '20',
+            sortBy: ['id:ASC', 'createdAt:DESC'],
+            search: 'white',
+            'filter.name': '$not:$eq:Kitty',
+            'filter.createdAt': ['$gte:2020-01-01', '$lte:2020-12-31'],
+        })
+
+        const result: PaginateQuery = decoratorfactory(null, context)
+
+        expect(result).toStrictEqual({
+            page: 1,
+            limit: 20,
+            sortBy: [
+                ['id', 'ASC'],
+                ['createdAt', 'DESC'],
+            ],
+            search: 'white',
+            searchBy: undefined,
+            path: 'http://localhost/items',
+            filter: {
+                name: '$not:$eq:Kitty',
+                createdAt: ['$gte:2020-01-01', '$lte:2020-12-31'],
+            },
+        })
+    })
+
+    it('should handle fastify defined query fields', () => {
+        const context = fastifyContextFactory({
             page: '1',
             limit: '20',
             sortBy: ['id:ASC', 'createdAt:DESC'],
