65e6ef87034de25563d6f14cf9163180ddd14b34
Vitalii Samofal <vitalii.samofal@softkit.dev>
2025-08-21 13:09:42 +0100

    feat: replaced HTML formatting with markdown as new versions of swagger wrapping HTML into code that looks weird, and added documentation for customisations

--- src/swagger/api-paginated-query.decorator.ts
+++ src/swagger/api-paginated-query.decorator.ts
@@ -3,60 +3,82 @@ import { ApiQuery } from '@nestjs/swagger'
 import { FilterComparator } from '../filter'
 import { FilterOperator, FilterSuffix, PaginateConfig } from '../paginate'
 import globalConfig from '../global-config'
+import { isNil } from '../helper'
 
 const DEFAULT_VALUE_KEY = 'Default Value'
 
+const allFilterSuffixes = Object.values(FilterSuffix).map((v) => v.toString())
+
 function p(key: string | 'Format' | 'Example' | 'Default Value' | 'Max Value', value: string) {
-    return `<p>
-             <b>${key}: </b> ${value}
-          </p>`
+    return `
+**${key}:** ${value}
+`
 }
 
 function li(key: string | 'Available Fields', values: string[]) {
-    return `<h4>${key}</h4><ul>${values.map((v) => `<li>${v}</li>`).join('\n')}</ul>`
+    return `**${key}**
+${values.map((v) => `- ${v}`).join('\n\n')}`
 }
 
 export function SortBy(paginationConfig: PaginateConfig<any>) {
+    const sortableColumnNotAvailable =
+        isNil(paginationConfig.sortableColumns) || paginationConfig.sortableColumns.length === 0
+
+    if (isNil(paginationConfig.defaultSortBy) && sortableColumnNotAvailable) {
+        // no sorting allowed or predefined
+        return undefined
+    }
+
     const defaultSortMessage = paginationConfig.defaultSortBy
         ? paginationConfig.defaultSortBy.map(([col, order]) => `${col}:${order}`).join(',')
-        : 'No default sorting specified, the result order is not guaranteed'
+        : 'No default sorting specified, the result order is not guaranteed if not provided'
 
     const sortBy = paginationConfig.sortableColumns.reduce((prev, curr) => {
         return [...prev, `${curr}:ASC`, `${curr}:DESC`]
     }, [])
+
+    const exampleValue = sortableColumnNotAvailable
+        ? 'Allowed sortable columns are not provided, only default sorting will be used'
+        : paginationConfig.sortableColumns
+              .slice(0, 2)
+              .map((col) => `sortBy=${col}:DESC`)
+              .join('&')
 
     return ApiQuery({
         name: 'sortBy',
         isArray: true,
         enum: sortBy,
         description: `Parameter to sort by.
-      <p>To sort by multiple fields, just provide query param multiple types. The order in url defines an order of sorting</p>
-      ${p('Format', 'fieldName:DIRECTION')}
-      ${p('Example', 'sortBy=id:DESC&sortBy=createdAt:ASC')}
-      ${p('Default Value', defaultSortMessage)}
-      ${li('Available Fields', paginationConfig.sortableColumns)}
-      `,
+To sort by multiple fields, just provide query param multiple types. The order in url defines an order of sorting
+${p('Format', '{fieldName}:{DIRECTION}')}
+${p('Example', exampleValue)}
+${p('Default Value', defaultSortMessage)}
+${li('Available Fields', paginationConfig.sortableColumns)}
+`,
         required: false,
         type: 'string',
     })
 }
 
-function Limit(paginationConfig: PaginateConfig<any>) {
+export function Limit(paginationConfig: PaginateConfig<any>) {
     return ApiQuery({
         name: 'limit',
         description: `Number of records per page.
-      ${p('Example', '20')}
-      ${p(DEFAULT_VALUE_KEY, paginationConfig?.defaultLimit?.toString() || globalConfig.defaultLimit.toString())}
-      ${p('Max Value', paginationConfig.maxLimit?.toString() || globalConfig.defaultMaxLimit.toString())}
 
-      If provided value is greater than max value, max value will be applied.
-      `,
+${p('Example', globalConfig.defaultLimit.toString())}
+
+${p(DEFAULT_VALUE_KEY, paginationConfig?.defaultLimit?.toString() || globalConfig.defaultLimit.toString())}
+
+${p('Max Value', paginationConfig.maxLimit?.toString() || globalConfig.defaultMaxLimit.toString())}
+
+If provided value is greater than max value, max value will be applied.
+`,
         required: false,
         type: 'number',
     })
 }
 
-function Select(paginationConfig: PaginateConfig<any>) {
+export function Select(paginationConfig: PaginateConfig<any>) {
     if (!paginationConfig.select) {
         return
     }
@@ -64,37 +86,45 @@ function Select(paginationConfig: PaginateConfig<any>) {
     return ApiQuery({
         name: 'select',
         description: `List of fields to select.
-      ${p('Example', paginationConfig.select.slice(0, Math.min(5, paginationConfig.select.length)).join(','))}
-      ${p(
-          DEFAULT_VALUE_KEY,
-          'By default all fields returns. If you want to select only some fields, provide them in query param'
-      )}
-      `,
+${p('Example', paginationConfig.select.slice(0, 5).join(','))}
+${p(
+    DEFAULT_VALUE_KEY,
+    'By default all fields returns. If you want to select only some fields, provide them in query param'
+)}
+`,
         required: false,
         type: 'string',
     })
 }
 
-function Where(paginationConfig: PaginateConfig<any>) {
+export function Where(paginationConfig: PaginateConfig<any>) {
     if (!paginationConfig.filterableColumns) return
 
     const allColumnsDecorators = Object.entries(paginationConfig.filterableColumns)
         .map(([fieldName, filterOperations]) => {
             const operations =
                 filterOperations === true || filterOperations === undefined
-                    ? [
-                          ...Object.values(FilterComparator),
-                          ...Object.values(FilterSuffix),
-                          ...Object.values(FilterOperator),
-                      ]
+                    ? [...Object.values(FilterOperator), ...Object.values(FilterSuffix)]
                     : filterOperations.map((fo) => fo.toString())
+
+            const operationsForExample =
+                operations
+                    .filter((v) => !allFilterSuffixes.includes(v))
+                    .sort()
+                    .slice(0, 2) || []
 
             return ApiQuery({
                 name: `filter.${fieldName}`,
                 description: `Filter by ${fieldName} query param.
-          ${p('Format', `filter.${fieldName}={$not}:OPERATION:VALUE`)}
-          ${p('Example', `filter.${fieldName}=$not:$like:John Doe&filter.${fieldName}=like:John`)}
-          ${li('Available Operations', operations)}`,
+${p('Format', `filter.${fieldName}={$not}:OPERATION:VALUE`)}
+
+${p(
+    'Example',
+    operationsForExample.length === 0
+        ? 'No filtering allowed'
+        : operationsForExample.map((v) => `filter.${fieldName}=${v}:John Doe`).join('&')
+)}
+${li('Available Operations', [...operations, ...Object.values(FilterComparator)])}`,
                 required: false,
                 type: 'string',
                 isArray: true,
@@ -105,45 +135,45 @@ function Where(paginationConfig: PaginateConfig<any>) {
     return applyDecorators(...allColumnsDecorators)
 }
 
-function Page() {
+export function Page() {
     return ApiQuery({
         name: 'page',
-        description: `Page number to retrieve.If you provide invalid value the default page number will applied
-        ${p('Example', '1')}
-        ${p(DEFAULT_VALUE_KEY, '1')}
-        `,
+        description: `Page number to retrieve. If you provide invalid value the default page number will applied
+${p('Example', '1')}
+${p(DEFAULT_VALUE_KEY, '1')}
+`,
         required: false,
         type: 'number',
     })
 }
 
-function Search(paginateConfig: PaginateConfig<any>) {
+export function Search(paginateConfig: PaginateConfig<any>) {
     if (!paginateConfig.searchableColumns) return
 
     return ApiQuery({
         name: 'search',
         description: `Search term to filter result values
-        ${p('Example', 'John')}
-        ${p(DEFAULT_VALUE_KEY, 'No default value')}
-        `,
+${p('Example', 'John')}
+${p(DEFAULT_VALUE_KEY, 'No default value')}
+`,
         required: false,
         type: 'string',
     })
 }
 
-function SearchBy(paginateConfig: PaginateConfig<any>) {
+export function SearchBy(paginateConfig: PaginateConfig<any>) {
     if (!paginateConfig.searchableColumns) return
 
     return ApiQuery({
         name: 'searchBy',
         description: `List of fields to search by term to filter result values
-        ${p(
-            'Example',
-            paginateConfig.searchableColumns.slice(0, Math.min(5, paginateConfig.searchableColumns.length)).join(',')
-        )}
-        ${p(DEFAULT_VALUE_KEY, 'By default all fields mentioned below will be used to search by term')}
-        ${li('Available Fields', paginateConfig.searchableColumns)}
-        `,
+${p(
+    'Example',
+    paginateConfig.searchableColumns.slice(0, Math.min(5, paginateConfig.searchableColumns.length)).join(',')
+)}
+${p(DEFAULT_VALUE_KEY, 'By default all fields mentioned below will be used to search by term')}
+${li('Available Fields', paginateConfig.searchableColumns)}
+`,
         required: false,
         isArray: true,
         type: 'string',
